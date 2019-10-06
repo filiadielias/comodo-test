a52daab482b5ea4cf9ad86e8b9df77d1e794bb05
Koinworks <filiadi.elias@koinworks.com>
2019-10-06 14:36:16 +0700

    add face direction string constant

--- model/consts.go
+++ model/consts.go
@@ -1,3 +1,17 @@
 package model
 
-const ()
+//face direction
+const (
+	NORTH string = "NORTH"
+	SOUTH string = "SOUTH"
+	WEST  string = "WEST"
+	EAST  string = "EAST"
+)
+
+//direction coordinate
+var (
+	FaceNorth = Coordinate{0, 1}
+	FaceEast  = Coordinate{1, 0}
+	FaceSouth = Coordinate{0, -1}
+	FaceWest  = Coordinate{-1, 0}
+)

03ab75d7429981e4174db4fb8a7c5bc25221880d
Koinworks <filiadi.elias@koinworks.com>
2019-10-06 14:36:55 +0700

    add new error for robot

--- model/errors.go
+++ model/errors.go
@@ -5,4 +5,8 @@ import "errors"
 //Error variables
 var (
 	ErrInvalidBoardSize = errors.New("invalid board size")
+
+	ErrCoordinateNotExists = errors.New("coordinate not exists")
+
+	ErrInvalidMove = errors.New("invalid move")
 )
